492d8a91218f76cdd532b46e7fe32609fa88cc88
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-11-28 11:58:17 -0700

    Made functions
    Created functions for fizzbuzz

--- js/site.js
+++ js/site.js
@@ -1,59 +1,82 @@
+// get values from the user. We need to get the fizz and the buzz value.
 
-// get the values from the Page
-// starts or controller function
 function getValues() {
-    // get values from the page
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+    // get the user values from the page
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    // validate input
-    // parse into integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    // parse for numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
-    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
-        // call generateNumbers
-        let numbers = generateNumbers(startValue, endValue);
-        // call displayNumbers
-        displayNumbers(numbers);
+    // check that the numbesr are integers
+    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        // call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        // call displayData
+        displayData(fbArray);
 
     } else {
-        alert("You must enter integers.")
+        alert("You must enter integers.");
     }
 }
 
-// generate numbers from startValue to the endValue
-// logic function
-function generateNumbers(startValue, endValue) {
-    let numbers = [];
+function fizzBuzz(fizzValue, buzzValue) {
+    // init the returnArray
+    let returnArray = [];
+    // loop from 1 - 100
+    for(let i = 1; i <= 100; i++) {
 
-    // we want to get all numbers from start to end
-    for(let i = startValue; i <= endValue; i++) {
-
-        // this will excute in a loop until index = endValue
-        numbers.push(i);
-    }
-    return numbers;
-}
-
-// display the numbers and mark even numbers bold
-// display or view functions
-function displayNumbers(numbers) {
-
-    let templateRows = "";
-
-    for(let i=0; i < numbers.length; i++) {
-        let number = numbers[i];
-
-        let className = "even";
-
-        if(number % 2 == 0) {
-            className = "even";
+        if(i % fizzValue == 0 && i % buzzValue == 0) {
+            returnArray.push('FizzBuzz');
+        } else if(i % fizzValue == 0) {
+            returnArray.push('Fizz');
+        } else if(i % buzzValue == 0) {
+            returnArray.push('Buzz');
         } else {
-            className = "odd";
+            returnArray.push(i);
         }
-
-        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
     }
-    document.getElementById("results").innerHTML = templateRows;
+    return returnArray;
+    // we need to check the current in three steps
+    // check to see if divisible by both 3 and 5
+    // if so push 'FizzBuzz' into an array and not the number
+
+    // check to see if divisible by fizz value 3
+    // if so push 'Fizz' into an array and not the number
+
+    // check to see if divisible by buzz value 5
+    // if so push 'Buzz' into an array and not the number
+
+    // if none then push the number into the array
+}
+
+function displayData(fbArray) {
+    // loop over the array and create a tablerow for each item
+
+    // get the table body element for each page
+    let tableBody = document.getElementById("results");
+
+    // get the template row
+    let templateRow = document.getElementById("fbTemplate");
+
+    // clear table first
+    tableBody.innerHTML = "";
+    
+    for(let i = 0; i < fbArray.length; i += 5) {
+
+        let tableRow = document.importNode(templateRow.content, true);
+
+        // grab use the to put into array
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].textContent = fbArray[i];
+        rowCols[1].textContent = fbArray[i+1];
+        rowCols[2].textContent = fbArray[i+2];
+        rowCols[3].textContent = fbArray[i+3];
+        rowCols[4].textContent = fbArray[i+4];
+
+        tableBody.appendChild(tableRow);
+    }
+
+    // add all the rows to the table
 }
